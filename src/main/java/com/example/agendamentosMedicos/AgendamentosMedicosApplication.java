c7187870fa3f5d8b193b195835c4ecc6b3251a1c
giotodsco <todescogiovanni18@gmail.com>
2026-02-19 19:34:33 -0300

    first commit

---
+++ src/main/java/com/example/agendamentosMedicos/AgendamentosMedicosApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendamentosMedicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentosMedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentosMedicosApplication.class, args);
+	}
+
+}
