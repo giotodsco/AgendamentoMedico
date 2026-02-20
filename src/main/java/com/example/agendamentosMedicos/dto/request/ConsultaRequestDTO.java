0bcf208b6fcd0cb2520a338724c2c5d44742dd98
giotodsco <todescogiovanni18@gmail.com>
2026-02-19 23:05:09 -0300

    refactor: remove o statusconsulta do contrutor do requestdto

--- src/main/java/com/example/agendamentosMedicos/dto/request/ConsultaRequestDTO.java
+++ src/main/java/com/example/agendamentosMedicos/dto/request/ConsultaRequestDTO.java
@@ -5,5 +5,5 @@ import com.example.agendamentosMedicos.model.Medico;
 import com.example.agendamentosMedicos.model.Paciente;
 import jakarta.persistence.*;
 
-public record ConsultaRequestDTO(Long idPaciente, Long idMedico, String data, String horario, StatusConsulta consulta) {
+public record ConsultaRequestDTO(Long idPaciente, Long idMedico, String data, String horario) {
 }
