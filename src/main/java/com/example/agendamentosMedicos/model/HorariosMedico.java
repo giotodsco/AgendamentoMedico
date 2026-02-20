e05e5c78b38d1342560e0bd31a05b43f2e3342cc
giotodsco <todescogiovanni18@gmail.com>
2026-02-20 20:38:11 -0300

    feat: cria a classe de horario do medico para adicionar horarios a um medico

---
+++ src/main/java/com/example/agendamentosMedicos/model/HorariosMedico.java
@@ -0,0 +1,42 @@
+package com.example.agendamentosMedicos.model;
+
+import com.example.agendamentosMedicos.dto.request.HorariosMedicosRequest;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_horarios_medico")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class HorariosMedico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String data;
+
+    private String horario;
+
+    private boolean isDisponivel = true;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    public HorariosMedico(HorariosMedicosRequest request, Medico medico){
+        //(Long medicoid, String data, String horario)
+        this.medico = medico;
+        this.data = request.data();
+        this.horario = request.horario();
+        this.isDisponivel = true;
+    }
+
+}
