3a1152da613db851981e4f80a69716b53f4e989d
giotodsco <todescogiovanni18@gmail.com>
2026-02-20 20:43:24 -0300

    refactor: adiciona a lista de horarios ao DTO

--- src/main/java/com/example/agendamentosMedicos/dto/response/MedicoResponseEspecialidadeDTO.java
+++ src/main/java/com/example/agendamentosMedicos/dto/response/MedicoResponseEspecialidadeDTO.java
@@ -2,17 +2,19 @@ package com.example.agendamentosMedicos.dto.response;
 
 import com.example.agendamentosMedicos.enums.Especialidade;
 import com.example.agendamentosMedicos.model.Consulta;
+import com.example.agendamentosMedicos.model.HorariosMedico;
 import com.example.agendamentosMedicos.model.Medico;
 
 import java.util.List;
 
-public record MedicoResponseEspecialidadeDTO(Long id, String name, Especialidade especialidade, String crm) {
+public record MedicoResponseEspecialidadeDTO(Long id, String name, Especialidade especialidade, String crm, List<HorariosMedicosSimples> list) {
     public MedicoResponseEspecialidadeDTO(Medico medico){
         this(
                 medico.getId(),
                 medico.getName(),
                 medico.getEspecialidade(),
-                medico.getCrm()
+                medico.getCrm(),
+                medico.getHorariosMedicos().stream().map(HorariosMedicosSimples::new).toList()
         );
     }
 }
