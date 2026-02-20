0051d8d95029a4593edf39dd91d729a3edb6d543
giotodsco <todescogiovanni18@gmail.com>
2026-02-20 20:42:33 -0300

    refactor: adiciona uma melhor validação dos et de consulta e horarios

--- src/main/java/com/example/agendamentosMedicos/dto/response/MedicoResponseDTO.java
+++ src/main/java/com/example/agendamentosMedicos/dto/response/MedicoResponseDTO.java
@@ -2,6 +2,7 @@ package com.example.agendamentosMedicos.dto.response;
 
 import com.example.agendamentosMedicos.enums.Especialidade;
 import com.example.agendamentosMedicos.model.Consulta;
+import com.example.agendamentosMedicos.model.HorariosMedico;
 import com.example.agendamentosMedicos.model.Medico;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
@@ -9,14 +10,15 @@ import jakarta.persistence.Enumerated;
 
 import java.util.List;
 
-public record MedicoResponseDTO(Long id, String name, Especialidade especialidade, String crm, List<Consulta> consultasListMedico) {
+public record MedicoResponseDTO(Long id, String name, Especialidade especialidade, String crm, List<Long> consultasIds, List<HorariosMedicosSimples> horarios) {
     public MedicoResponseDTO(Medico medico){
         this(
                 medico.getId(),
                 medico.getName(),
                 medico.getEspecialidade(),
                 medico.getCrm(),
-                medico.getConsultasListMedico()
+                medico.getConsultasListMedico().stream().map(Consulta::getId).toList(),
+                medico.getHorariosMedicos().stream().map(HorariosMedicosSimples::new).toList()
         );
     }
 }
