afb4c1f00387cc906afb3f3f633f462bc530c5d4
giotodsco <todescogiovanni18@gmail.com>
2026-02-20 20:42:55 -0300

    feat: cria o paciente controller

---
+++ src/main/java/com/example/agendamentosMedicos/controller/PacienteController.java
@@ -0,0 +1,9 @@
+package com.example.agendamentosMedicos.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paciente")
+public class PacienteController {
+}
