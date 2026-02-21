0c59bd5bc0d789f4dc7a8698c8306b026aaf7d82
giotodsco <todescogiovanni18@gmail.com>
2026-02-20 21:21:20 -0300

    fix: adiciona validações para datas e horas

--- src/main/java/com/example/agendamentosMedicos/service/MedicoHorarioService.java
+++ src/main/java/com/example/agendamentosMedicos/service/MedicoHorarioService.java
@@ -11,6 +11,10 @@ import com.example.agendamentosMedicos.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 public class MedicoHorarioService {
     @Autowired
@@ -20,6 +24,22 @@ public class MedicoHorarioService {
 
     public HorariosMedicosSimples createHorario(HorariosMedicosRequest request, Long idMedico){
         Medico medicoParaColocarHorario = repository.findById(idMedico).orElseThrow(() -> new RuntimeException("Médico não encontrado"));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataConsulta = LocalDate.parse(request.data(), formatter);
+
+        boolean horarioJaExiste = medicoParaColocarHorario.getHorariosMedicos().stream()
+                .anyMatch
+                        (hora -> hora.getData().equals(request.data()) && hora.getHorario().equals(request.horario()));
+
+        if(dataConsulta.isBefore(LocalDate.now())){
+            throw new RuntimeException("Data deve ser futura!");
+        }
+
+        if(horarioJaExiste){
+            throw new RuntimeException("Medico ja tem consulta nesse dia e hora");
+        }
+
         HorariosMedico horariosMedico = new HorariosMedico(request, medicoParaColocarHorario);
 
         HorariosMedico horariosMedicoSalvo = medicoHorarioRepository.save(horariosMedico);
